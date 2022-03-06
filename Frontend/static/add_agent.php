908e9ee115036b8150eb45a0611b6fc26c95c744
MubeenAhmadShaikh <mubeen.shaikh7@yahoo.com>
2022-03-07 01:36:53 +0530

    Adding agents UI update

--- Frontend/static/add_agent.php
+++ Frontend/static/add_agent.php
@@ -34,29 +34,27 @@
                   <!-- First row -->
                   <div class="row"> 
                     <div class="w-50 mb-3">
-                      <label for="exampleInputEmail1" class="form-label">First Name</label>
-                      <input type="email" class="form-control" name="afirst_name" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <label  class="form-label">First Name</label>
+                      <input type="text" class="form-control" name="afirst_name" aria-describedby="emailHelp">
                     </div>
                     <div class="w-50 mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Last Name</label>
-                      <input type="email" class="form-control" name="alast_name" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <label  class="form-label">Last Name</label>
+                      <input type="text" class="form-control" name="alast_name"  aria-describedby="emailHelp">
                     </div>
                     
                   </div>
                   <!-- Second row -->
                   <div class="row"> 
                     <div class="w-50 mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Gender</label>
+                      <label  class="form-label">Gender</label>
                       <select class="form-select" aria-label="Default select example" name="gender">
                         <option selected>Select gender</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
-                        <option value="Trans">Trans</option>
-                        <option value="Prefer not to say">Prefer not to say</option>
                       </select>
                     </div>
                     <div class="w-50 mb-3">
-                      <label for="exampleInputEmail1" class="form-label">KYC</label>
+                      <label class="form-label">KYC</label>
                       <select class="form-select" aria-label="Default select example" name="kyc">
                         <option selected>Select KYC Status</option>
                         <option value="yes">Yes</option>
@@ -67,12 +65,12 @@
                   <!-- Third row -->
                 <div class="row"> 
                 <div class="w-50 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Adhaar Number</label>
-                    <input type="text" class="form-control" name="adhaar" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <label  class="form-label">Adhaar Number</label>
+                    <input type="text" class="form-control" name="adhaar" aria-describedby="emailHelp">
                   </div>
                   <div class="w-50 mb-3">
-                    <label for="exampleInputEmail1" class="form-label">PAN Number</label>
-                    <input type="text" class="form-control" name="pan" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <label  class="form-label">PAN Number</label>
+                    <input type="text" class="form-control" name="pan"  aria-describedby="emailHelp">
                   </div>
                 </div>
                 
@@ -94,11 +92,7 @@
     <script src="js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
-      <script type="text/javascript">
-      $(function(){
-            $('#datepicker').datepicker();
-      });
-    </script>
+      
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
   </body>
 </html>
--- Frontend/static/agents.php
+++ Frontend/static/agents.php
@@ -36,15 +36,52 @@
             </div>
 
             <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <div class="card-header">
-                    <h5 class="card-title mb-0">Empty card</h5>
-                  </div>
-                  <div class="card-body"></div>
-                </div>
-              </div>
-            </div>
+						<div class="col-12 col-xl-12">
+							<div class="card">
+								
+								<table class="table">
+									<thead>
+										<tr>
+											<th >Name</th>
+											<th >Gender</th>
+											<th >KYC Status</th>
+											<th >Adhaar Number</th>
+											<th >PAN Number</th>
+											<th class="d-none d-md-table-cell" >Date of Birth</th>
+											<th>Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Vanessa Tucker</td>
+											<td>Female</td>
+											<td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+											<td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+											<td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+                      <td class="d-none d-md-table-cell">May 15, 1948</td>
+											<td class="table-action">
+												<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+												<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+											</td>
+										</tr>
+										<tr>
+											<td>Vanessa Tucker</td>
+											<td>Female</td>
+											<td ><i class="align-middle text-success" data-feather="check"></i></td>
+											<td >2221 3333 2222 4444</td>
+											<td >2221 3333 2222 4444</td>
+                      <td class="d-none d-md-table-cell">May 15, 1948</td>
+											<td class="table-action">
+												<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+												<a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+											</td>
+										</tr>
+										
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
           </div>
         </main>
 
