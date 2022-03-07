fdd2e660f27ea04d10eb7a4f5bdcc2c230d4943d
MubeenAhmadShaikh <mubeen.shaikh7@yahoo.com>
2022-03-08 01:03:57 +0530

    Loans AddLoans UI

---
+++ Frontend/static/add_loan.php
@@ -0,0 +1,169 @@
+<?php
+include('include/connection.php');
+error_reporting(0);
+session_start();
+if ($_SESSION['username']==false) {
+   header("Location:sign_in.php");
+}
+?>
+
+
+<!-- #TODO
+1. Add the names of each field according to you
+-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="author" content="LenaDenaBank" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+    <link rel="canonical" href="https://demo-basic.adminkit.io/pages-blank.html" />
+
+    <title>LenaDenaBank | Agents</title>
+
+    <link href="css/app.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet" />
+  </head>
+
+  <body>
+  <div class="wrapper">
+    <?php include ('include/sidebar.php')?>
+
+      <div class="main">
+      <?php include ('include/header.php')?>
+
+        <main class="content">
+          <div class="container-fluid p-0">
+              <h1 class="h3 mb-3">Add Loans</h1>
+              <div class="row">
+              <div class="col-12">
+                <div class="card">
+                <form class="m-5" method="post" name='add'>
+                  <!-- First row -->
+                  <div class="row"> 
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Loan Name</label>
+                      <input type="text" class="form-control" name="pfirst_name" aria-describedby="emailHelp">
+                    </div>
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Contact number</label>
+                      <input type="phone" class="form-control" name=""  >
+                    </div>
+                    
+                  </div>
+                  <!-- Second row -->
+                  <div class="row"> 
+                  <div class="w-50 mb-3">
+                        <label  class="form-label">Date of birth</label>
+                        <input type="date" class="form-control" name="dob"  >
+                    </div> 
+                    <div class="w-50 mb-3">
+                      <label class="form-label">KYC</label>
+                      <select class="form-select" aria-label="Default select example" name="kyc">
+                        <option selected>Select KYC Status</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                      </select>
+                    </div>
+                  </div>
+                  <!-- Third row -->
+                <div class="row"> 
+                <div class="w-50 mb-3">
+                    <label  class="form-label">Adhaar Number</label>
+                    <input type="text" class="form-control" name="adhaar_number" aria-describedby="emailHelp">
+                  </div>
+                  <div class="w-50 mb-3">
+                    <label  class="form-label">PAN Number</label>
+                    <input type="text" class="form-control" name="pan_number"  aria-describedby="emailHelp">
+                  </div>
+                </div>
+                <!-- fourth row -->
+                <div class="row"> 
+                    
+                             
+                </div>
+                  <!-- Fifth row -->
+                <div class="row"> 
+                <div class="w-50 mb-3">
+                        <label  class="form-label">Occupation</label>
+                        <input type="phone" class="form-control" name=""  >
+                    </div>
+                    
+                    <div class="w-50 mb-3">
+                        <label  class="form-label">City</label>
+                        <input type="decimal" class="form-control" name="" >
+                    </div>
+                    
+                  </div>
+                  <!-- sixth row -->
+                  <div class="row"> 
+                  <div class="w-50 mb-3">
+                      <label  class="form-label">Address</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Description</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                     
+                    </div>          
+                </div>
+                
+                  <!-- seventh row -->
+                
+                <div class="row"> 
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Loan Amount</label>
+                      <input type="text" class="form-control" name="">
+                    </div>
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Loan Intrest</label>
+                      <input type="decimal" class="form-control" name="" >
+                    </div>
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Loan duration</label>
+                      <input type="text" class="form-control" name="" >
+                    </div>
+                    
+                  </div>
+                <!-- eighth row -->
+                <div class="row"> 
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Agent id</label>
+                      <select class="form-select" aria-label="Default select example" name="kyc">
+                        <option selected>Select Agents</option>
+                        <option value="Hans">Hans</option>
+                        <option value="khizar">Khizar</option>
+                      </select>
+                    </div>
+                    <div class="w-50 mb-3">
+                      <label  class="form-label">Agent commission</label>
+                      <input type="decimal" class="form-control" name=""  >
+                    </div>          
+                </div>
+                
+  
+  
+  <button type="submit" class="btn btn-primary" name="add">Submit</button>
+</form>
+</div>
+</div>
+</div>
+              
+          </div>
+        </main>
+
+        <?php include ('include/footer.php')?>
+      </div>
+    </div>
+    
+    <script src="js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+      
+  </body>
+</html>
--- Frontend/static/loans.php
+++ Frontend/static/loans.php
@@ -8,9 +8,11 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-
-    <title>LenaDenaBank | Loans</title>
-
+    <title>LenaDenaBank | Party</title>
+    <!-- CDN till demo -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<!-- CDN till demo -->
     <link href="css/app.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet" />
   </head>
@@ -23,25 +25,277 @@
       <?php include ('include/header.php')?>
 
         <main class="content">
-          <!-- 
-            Loans status
-            
-            <td><span class="badge bg-success">Active</span></td>
-           -->
           <div class="container-fluid p-0">
-            <h1 class="h3 mb-3">Loans</h1>
-
             <div class="row">
-              <div class="col-12">
+              <div class="col-xl-12 partyTable">
+                <div class="card">
+                  <div class="card-header pb-0">
+                    <div class="card-actions float-end">
+                      <div class="dropdown position-relative">
+                      <a class="btn btn-primary" href="add_loan.php" role="button"><i class="align-middle" data-feather="plus"></i>Add Loans</a>
+
+                        <div class="dropdown-menu dropdown-menu-end">
+                          <a class="dropdown-item" href="#">Action</a>
+                          <a class="dropdown-item" href="#">Another action</a>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                      </div>
+                    </div>
+                    <h5 class="card-title mb-0">Loans</h5>
+                  </div>
+                  <div class="card-body">
+                    <table class="table table-striped" style="width: 100%">
+                      <thead>
+                        <tr >
+                          
+
+											<th>Id</th>
+											<th>Name</th>
+											<th>Amount</th>
+											<th>Interest</th>
+											<th>Duration</th>
+											<th >Agent</th>
+											<th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr id="party1">
+                          
+                          <td>L1</td>
+                          <td>Car Loan</td>
+                          <td>200000</td>
+                          <td>10%</td>
+                          <td>2 years</td>
+                          <td>Robert Kioski (RK1)</td>
+                          <td class="table-action d-flex justify-content-around">
+																<a href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+																<a href="#" class="text-danger" data-bs-toggle="modal" data-bs-target="#myModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+																
+														</td>
+                        </tr>
+                        <tr >
+                          
+                          <td>L1</td>
+                          <td>Home Loan</td>
+                          <td>20000</td>
+                          <td>10%</td>
+                          <td>2 years</td>
+                          <td>Robert Kioski (RK1)</td>
+                          <td class="table-action d-flex justify-content-around">
+																<a href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+																<a href="#" class="text-danger" data-bs-toggle="modal" data-bs-target="#myModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+																
+														</td>
+                        </tr>
+                        <tr >
+                          
+                          <td>L1</td>
+                          <td>Education Loan</td>
+                          <td>20000</td>
+                          <td>10%</td>
+                          <td>2 years</td>
+                          <td>Robert Kioski (RK1)</td>
+                          <td class="table-action d-flex justify-content-around">
+																<a href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+																<a href="#" class="text-danger" data-bs-toggle="modal" data-bs-target="#myModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+																
+														</td>
+                        </tr>
+                        
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-xl-4 d-none partyDetails">
                 <div class="card">
                   <div class="card-header">
-                    <h5 class="card-title mb-0">Empty card</h5>
+                    
+                    <h5 class="card-title mb-0">Car Loan</h5>
                   </div>
-                  <div class="card-body"></div>
+                  <div class="card-body">
+                    
+
+                    <table class="table table-sm  mb-4">
+                      <tbody>
+                        <tr>
+                          <th>Loan Id</th>
+                          <td>L1</td>
+                        </tr>
+                        <tr>
+                          <th>Serial Number</th>
+                          <td>101</td>
+                        </tr>
+                        <tr>
+                          <th>Name</th>
+                          <td>Car Loan</td>
+                        </tr>
+                        <tr>
+                          <th>Occupation</th>
+                          <td>Contractor</td>
+                        </tr>
+                        <tr>
+                          <th>Description</th>
+                          <td>Loan description will be mentioned here</td>
+                        </tr>
+                        <tr>
+                          <th>Amount</th>
+                          <td>200000</td>
+                        </tr>
+                        <tr>
+                          <th>Sanction date</th>
+                          <td>12/02/2020</td>
+                        </tr>
+                        <tr>
+                          <th>Duration</th>
+                          <td>2 years</td>
+                        </tr>
+                        <tr>
+                          <th>Interest</th>
+                          <td>10%</td>
+                        </tr>
+                        
+                        <tr>
+                          <th>Agent</th>
+                          <td>Robert Kioski (RK1)</td>
+                        </tr>
+                        <tr>
+                          <th>Agent Commission</th>
+                          <td>3%</td>
+                        </tr>
+                        <tr>
+                          <th>Phone</th>
+                          <td>+1234123123123</td>
+                        </tr>
+                        <tr>
+                          <th>KYC Status</th>
+                          <td><span class="badge bg-success">Completed</span></td>
+                        </tr>
+                        <tr>
+                          <th>Adhaar Number</th>
+                          <td>222234444555</td>
+                        </tr>
+                        <tr>
+                          <th>PAN Number</th>
+                          <td>222234444555</td>
+                        </tr>
+                        
+                        <tr>
+                          <th>City</th>
+                          <td>Mumbai</td>
+                        </tr>
+                        <tr>
+                          <th>Address</th>
+                          <td>12, A wing, Abc Apartment, xyz city,1111 </td>
+                        </tr>
+                        
+                      </tbody>
+                    </table>
+
+                    <strong>Recent Loans</strong>
+
+                    <ul class="timeline mt-2 mb-0">
+                      <li class="timeline-item">
+                        <strong>90 lakhs</strong>
+                        <span class="float-end text-muted text-sm">1 month ago</span>
+                        <p>Loan description will be mentioned here</p>
+                      </li>
+                      <li class="timeline-item">
+                        <strong>20 lakhs</strong>
+                        <span class="float-end text-muted text-sm">3 month ago</span>
+                        <p>Loan description will be mentioned here</p>
+                      </li>
+                      
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+        </main>
+
+        <!-- <main class="content">
+          <div class="container-fluid p-0">
+            <div class="d-flex flex-row justify-content-between">
+
+              <h1 class="h3 mb-3">Party</h1>
+              <div>
+
+                <a class="btn btn-primary" href="add_party.php" role="button"><i class="align-middle" data-feather="plus"></i>Add Party</a>
+              </div>
+            </div>
+		
+            <div class="row">
+						<div class="col-12 col-xl-12">
+							<div class="card">
+								
+								<table class="table">
+									<thead>
+										<tr>
+											<th>Acc. No.</th>
+											<th>Name</th>
+											<th>Balance</th>
+											<th>Agent Id</th>
+											<th>Contact</th>
+											<th >Acc. Opening date</th>
+											<th class="d-none d-md-table-cell" >Date of Birth</th>
+											<th>Actions</th>
+										</tr>
+									</thead>
+									<tbody>
+										
+												
+												<tr>
+													<td>123</td>
+													<td>Abc</td>
+													<td>Male</td>
+                          <td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+                          <td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+                          <td ><i class="align-middle text-danger" data-feather="alert-circle"></i></td>
+                          <td >12/2/2021</td>
+															<td class="table-action d-flex justify-content-around">
+																<a href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 align-middle"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
+																<a href="#" class="text-danger" data-bs-toggle="modal" data-bs-target="#myModal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
+																
+														</td>
+													
+											
+										</tr>
+										
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+          </div> -->
+		  
+		  <!-- Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Delete Party</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Removing Loan will remove all its details and data that cannot be retrieved please confirm to delete or cancel
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" name="delete" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+        <button type="button" class="btn btn-success" data-bs-dismiss="modal">Cancel</button>
+      </div>
+
+    </div>
+  </div>
+</div>
         </main>
 
         <?php include ('include/footer.php')?>
@@ -49,5 +303,24 @@
     </div>
 
     <script src="js/app.js"></script>
+    <script>
+      let party= document.getElementById('party1');
+      let partyTable= document.querySelector('.partyTable');
+      let partyDetails= document.querySelector('.partyDetails');
+      
+      party.addEventListener('click',abc)
+      function abc(){
+        if(partyTable.classList.contains('col-xl-12')){
+          partyTable.classList.remove('col-xl-12');
+          partyTable.classList.add('col-xl-8');
+          partyDetails.classList.remove('d-none');
+        }else{
+          partyTable.classList.add('col-xl-12');
+          partyTable.classList.remove('col-xl-8');
+          partyDetails.classList.add('d-none');
+          
+        }
+      }
+      </script>
   </body>
 </html>
