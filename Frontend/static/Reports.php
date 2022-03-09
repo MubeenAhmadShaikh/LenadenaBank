d57b7dcfd3cab52d40980adeeeb64efcd6b2fd49
hansraj1999 <deghun@gmail.com>
2022-03-09 22:35:45 +0530

    tp

--- Frontend/static/Reports.php
+++ Frontend/static/Reports.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="author" content="LenaDenaBank" />
 
+
     <link rel="preconnect" href="https://fonts.gstatic.com" />
+
     <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
     <link rel="canonical" href="https://demo-basic.adminkit.io/pages-blank.html" />
