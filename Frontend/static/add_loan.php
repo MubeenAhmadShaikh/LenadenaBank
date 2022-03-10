f1edb9d61ea965abff86f8b58b26041aa6814a8c
ShaikhKhizar451 <khizarsajid@gmail.com>
2022-03-11 01:23:15 +0530

    add_loan and add_party Search funtionality

--- Frontend/static/add_loan.php
+++ Frontend/static/add_loan.php
@@ -124,6 +124,10 @@ if (isset($_POST['add'])) {
 
     <link href="css/app.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet" />
+     
+    <link href="css/select2.min.css" rel="stylesheet" type="text/css" /> 
+    <script src="js/jquery.min.js"></script> 
+    <script src="js/select2.min.js"></script>
   </head>
 
   <body>
@@ -230,7 +234,7 @@ if (isset($_POST['add'])) {
                 <div class="row"> 
                     <div class="w-50 mb-3">
                       <label  class="form-label">Agent id</label>
-                      <select class="form-select" aria-label="Default select example" name="agentId">
+                      <select class="form-select" aria-label="Default select example" name="agentId" id="agentId">
                        <option selected>Select Agents </option>
                         <option name = 'none'>Select This If No Agent Is Req</option>
 
@@ -275,7 +279,11 @@ if (isset($_POST['add'])) {
     
     <script src="js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
+    <script>
+      $(document).ready(function(){
+        $("#agentId").select2();
+        });
+    </script>
       
   </body>
 </html>
