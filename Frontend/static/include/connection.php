ebeec4bf19cec1ebdbae27d69585a229a20b3816
hansraj1999 <deghun@gmail.com>
2022-03-19 16:03:48 +0530

    date inside connection.php

--- Frontend/static/include/connection.php
+++ Frontend/static/include/connection.php
@@ -5,6 +5,8 @@ $servername = "localhost:3406";
 $username = "root";
 $password = "";
 $dbname = "bank";
+date_default_timezone_set('Asia/Calcutta');
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
     printf("Connot connect to the database: %s\n", mysqli_connect_error());
